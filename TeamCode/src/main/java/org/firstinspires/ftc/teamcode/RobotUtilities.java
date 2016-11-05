273f81960f1767a93a581b8819980f2c92d51993
BlackTigers <blacktigersftc@gmail.com>
2016-11-05 13:27:46 +0200

    Started Teleop and Debugging

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotUtilities.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by user on 05/11/2016.
+ */
+
+public class RobotUtilities {
+
+    public static double normalizePower(double power) {
+        return Math.pow(power,3);
+    }
+
+}
