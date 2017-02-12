e359a1c0125578dd404ba49e1a4d5b9127e00f19
BlackTigers <blacktigersftc@gmail.com>
2017-02-12 21:44:30 +0200

    New Auto and minor changes in RobotUtilities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlackTigersHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlackTigersHardware.java
@@ -13,8 +13,8 @@ public class BlackTigersHardware
     public DcMotor  collectionMotor = null;
     public DcMotor  reloadingMotor = null;
     public DcMotor  shootingMotor = null;
-    public DcMotor  ballMotorLeft = null;
-    public DcMotor  ballMotorRight = null;
+//    public DcMotor  ballMotorLeft = null;
+//    public DcMotor  ballMotorRight = null;
     public GyroSensor gyro = null;
 
 
@@ -38,8 +38,8 @@ public class BlackTigersHardware
         collectionMotor = hwMap.dcMotor.get("collection_motor");
         reloadingMotor = hwMap.dcMotor.get("reloading_motor");
         shootingMotor = hwMap.dcMotor.get("shooting_motor");
-        ballMotorRight = hwMap.dcMotor.get("ball_motor_right");
-        ballMotorLeft = hwMap.dcMotor.get("ball_motor_left");
+//        ballMotorRight = hwMap.dcMotor.get("ball_motor_right");
+//        ballMotorLeft = hwMap.dcMotor.get("ball_motor_left");
         gyro = hwMap.gyroSensor.get("gyro");
 
         leftMotor.setDirection(DcMotor.Direction.FORWARD);
@@ -47,24 +47,24 @@ public class BlackTigersHardware
         collectionMotor.setDirection(DcMotor.Direction.REVERSE);
         reloadingMotor.setDirection(DcMotor.Direction.REVERSE);
         shootingMotor.setDirection(DcMotor.Direction.FORWARD);
-        ballMotorRight.setDirection(DcMotor.Direction.FORWARD);
-        ballMotorLeft.setDirection(DcMotor.Direction.REVERSE);
+//        ballMotorRight.setDirection(DcMotor.Direction.FORWARD);
+//        ballMotorLeft.setDirection(DcMotor.Direction.REVERSE);
 
         shootingMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         collectionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         reloadingMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ballMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ballMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        ballMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        ballMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftMotor.setPower(0);
         rightMotor.setPower(0);
         collectionMotor.setPower(0);
         reloadingMotor.setPower(0);
         shootingMotor.setPower(0);
-        ballMotorRight.setPower(0);
-        ballMotorLeft.setPower(0);
+//        ballMotorRight.setPower(0);
+//        ballMotorLeft.setPower(0);
 
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -72,8 +72,8 @@ public class BlackTigersHardware
         reloadingMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         collectionMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         collectionMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        ballMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        ballMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        ballMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        ballMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
 
